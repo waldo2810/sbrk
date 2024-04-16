d4bc01fe08aa5e61523b89a0677c9480689711f3
Waldo Sobrino <waldo.sobrino@republicnetwork.es>
2024-04-16 11:26:07 -0500

    fix: redux persist action serialized

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistReducer, persistStore } from "redux-persist";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import taskReducer from "./slices/task-slice";
 
@@ -12,6 +21,12 @@ const persistedReducer = persistReducer(persistConfig, taskReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: process.env.NODE_ENV !== "production",
 });
 
