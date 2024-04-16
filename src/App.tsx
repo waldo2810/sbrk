bf9d4f32baf9b4b0223c079a80c1475730ad7d04
Waldo Sobrino <waldo.sobrino@republicnetwork.es>
2024-04-15 21:07:17 -0500

    feat: boilerplate app

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,61 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Pencil, Plus, Trash } from "lucide-react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen w-full flex items-center">
+      <div className="w-[80%] h-[80%] mx-auto">
+        <h1 className="uppercase font-black text-[#656582] text-3xl pb-10">
+          Todo List
+        </h1>
+        <div className="flex items-center justify-between w-full my-2">
+          <button className="flex items-center gap-1 text-white bg-[#626FEF] px-3 py-2 rounded-md">
+            <Plus />
+            Add Task
+          </button>
+          <div className="space-x-2">
+            <input
+              type="text"
+              className="bg-[#CCCDDF] px-5 py-2 mr-2 rounded-md"
+            />
+            <select className="bg-[#CCCDDF] px-2 py-2 rounded-md">
+              <option>Sort by</option>
+              <option>Oldest</option>
+              <option>Most recent</option>
+              <option>Name (A-Z)</option>
+              <option>Name (Z-A)</option>
+            </select>
+            <select className="bg-[#CCCDDF] px-2 py-2 rounded-md">
+              <option>Show All</option>
+              <option>Show To do</option>
+              <option>Show Completed</option>
+            </select>
+          </div>
+        </div>
+        <div className="flex flex-col w-full p-4 bg-[#EDEDF7] rounded-md space-y-4">
+          <div className="flex items-center justify-between bg-white p-4 rounded-md cursor-grab">
+            <div className="flex gap-2 items-center">
+              <input
+                type="checkbox"
+                className="w-8 h-8 checked:font-thin bg-[#CCCDDF]"
+              />
+              <div className="flex flex-col">
+                <span>Create a react project</span>
+                <span className="text-gray-500">5:23 AM, 01/06/2022</span>
+              </div>
+            </div>
+            <div className="flex gap-2">
+              <div className="bg-[#EFEFEF] rounded-md p-2">
+                <Trash fill="#555555" className="text-[#555555]" />
+              </div>
+              <div className="bg-[#EFEFEF] rounded-md p-2">
+                <Pencil fill="#555555" className="text-[#555555]" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1 className="text-3xl font-bold underline">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
