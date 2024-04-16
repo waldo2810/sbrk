9d9b6d078e10b617a09c81d428de682cc305b5f2
Waldo Sobrino <waldo.sobrino@republicnetwork.es>
2024-04-16 11:27:59 -0500

    feat: add currentOrder state

--- src/redux/slices/task-slice.ts
+++ src/redux/slices/task-slice.ts
@@ -3,10 +3,12 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 interface InitialState {
   tasks: Task[];
+  currentOrder: number;
 }
 
 const initialState: InitialState = {
   tasks: [],
+  currentOrder: 0,
 };
 
 const taskSlice = createSlice({
@@ -15,12 +17,17 @@ const taskSlice = createSlice({
   reducers: {
     createTask: (state, action: PayloadAction<Task>) => {
       state.tasks.push(action.payload);
+      state.currentOrder++;
     },
     deleteTask: (state, action) => {
       state.tasks = state.tasks.filter((task) => task.id === action.payload);
+      state.currentOrder--;
+    },
+    reOrderTasks: (state, action) => {
+      state.tasks = action.payload;
     },
   },
 });
 
-export const { createTask, deleteTask } = taskSlice.actions;
+export const { createTask, deleteTask, reOrderTasks } = taskSlice.actions;
 export default taskSlice.reducer;
